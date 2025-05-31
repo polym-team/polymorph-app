6c04440a28a6505a2cd7f1143b775e24a6e75694
박윤서 <ys@bag-yunseoui-MacBookPro.local>
2025-05-31 15:50:47 +0900

    저장 목록 버튼 UI 수정

--- apps/jibsayo/src/features/transaction-search/ui/FavoriteRegionList.tsx
+++ apps/jibsayo/src/features/transaction-search/ui/FavoriteRegionList.tsx
@@ -17,44 +17,26 @@ export function FavoriteRegionList() {
 
   return (
     <div className="flex gap-x-1">
-      <div className="border-input bg-background flex rounded-md border">
-        <Button
-          variant="ghost"
-          size="sm"
-          onClick={() => handleSelectRegion('서울시 강동구')}
-          className="rounded-r-none border-0 px-3 py-1.5 text-sm"
-        >
-          서울시 강동구
-        </Button>
-        <Button
-          variant="ghost"
-          size="sm"
-          onClick={() => handleRemoveRegion('서울시 강동구')}
-          className="h-full min-w-0 rounded-l-none border-0 px-2 py-1.5"
-          aria-label="서울시 강동구 삭제"
-        >
-          <X className="h-3 w-3" />
-        </Button>
-      </div>
-      <div className="border-input bg-background flex items-center rounded-md border">
-        <Button
-          variant="ghost"
-          size="sm"
-          onClick={() => handleSelectRegion('서울시 동작구')}
-          className="rounded-r-none border-0 px-3 py-1.5 text-sm"
-        >
-          서울시 동작구
-        </Button>
-        <Button
-          variant="ghost"
-          size="sm"
-          onClick={() => handleRemoveRegion('서울시 동작구')}
-          className="h-full min-w-0 rounded-l-none border-0 px-2 py-1.5"
-          aria-label="서울시 동작구 삭제"
-        >
-          <X className="h-3 w-3" />
-        </Button>
-      </div>
+      {['서울시 강동구', '서울시 동작구'].map(text => (
+        <div className="border-input bg-background flex rounded-md border">
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => handleSelectRegion(text)}
+            className="rounded-r-none border-0 px-3 py-1.5 text-sm"
+          >
+            <span className="translate-y-[-0.5px]">{text}</span>
+          </Button>
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => handleRemoveRegion(text)}
+            className="h-full min-w-0 rounded-l-none border-0 px-2 py-1.5"
+          >
+            <X className="h-3 w-3" />
+          </Button>
+        </div>
+      ))}
     </div>
   );
 }
