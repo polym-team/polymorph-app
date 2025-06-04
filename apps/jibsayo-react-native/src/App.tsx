0237702b409e174a5b116c0a009a66c52c4ed9d0
HungSun LIM <majac6@gmail.com>
2025-06-04 09:34:53 +0900

    feat: add jibsayo-react-native app

---
+++ apps/jibsayo-react-native/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Welcome to jibsayo-react-native!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
