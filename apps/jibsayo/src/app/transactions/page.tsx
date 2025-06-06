03b9be9b948b9af9d8f32b05f667e21cb57504e8
pys99pys <pys99pys@gmail.com>
2025-06-07 03:16:37 +0900

    서버 컴포넌트 작업중

--- apps/jibsayo/src/app/transactions/page.tsx
+++ apps/jibsayo/src/app/transactions/page.tsx
@@ -1,30 +1,59 @@
-import { TransactionList } from '@/features/transaction-list';
+import { TransactionList } from '@/features/transaction-list/components/TransactionList';
 import { TransactionSearch } from '@/features/transaction-search';
+
+import { Suspense } from 'react';
 
 import { getTransactions } from './service';
 
-export default async function TransactionsPage({
-  searchParams,
-}: {
-  searchParams: { regionCode?: string; tradeDate?: string };
-}) {
+interface Props {
+  searchParams: {
+    regionCode?: string;
+    tradeDate?: string;
+  };
+}
+
+export default async function TransactionsPage({ searchParams }: Props) {
   const { regionCode, tradeDate } = searchParams;
-
-  if (!regionCode || !tradeDate) {
-    return null;
-  }
-
-  const data = await getTransactions(regionCode, tradeDate);
-
-  if (!data) {
-    return null;
-  }
 
   return (
     <main className="container mx-auto py-8">
       <div className="flex flex-col gap-y-5">
         <TransactionSearch />
-        <TransactionList regionCode={regionCode} data={data} />
+
+        <Suspense
+          fallback={
+            <TransactionList
+              regionCode={regionCode}
+              data={{ list: [], count: 0 }}
+              isLoading={true}
+              isFetched={false}
+            />
+          }
+        >
+          <div>
+            regionCode: {regionCode} / tradeDate: {tradeDate}
+          </div>
+          {regionCode && tradeDate ? (
+            <TransactionList
+              regionCode={regionCode}
+              data={
+                (await getTransactions(regionCode, tradeDate)) || {
+                  list: [],
+                  count: 0,
+                }
+              }
+              isLoading={false}
+              isFetched={true}
+            />
+          ) : (
+            <TransactionList
+              regionCode={regionCode}
+              data={{ list: [], count: 0 }}
+              isLoading={false}
+              isFetched={false}
+            />
+          )}
+        </Suspense>
       </div>
     </main>
   );
