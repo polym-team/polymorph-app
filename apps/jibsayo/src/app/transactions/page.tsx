000d029639e378aa3d6807d2fa1579b2375336d7
pys99pys <pys99pys@gmail.com>
2025-06-07 02:55:48 +0900

    실거래가 조회 서버 컴포넌트 사용

--- apps/jibsayo/src/app/transactions/page.tsx
+++ apps/jibsayo/src/app/transactions/page.tsx
@@ -1,21 +1,31 @@
 import { TransactionList } from '@/features/transaction-list';
 import { TransactionSearch } from '@/features/transaction-search';
 
-import { Suspense } from 'react';
+import { getTransactions } from './service';
 
-export default function TransactionsPage() {
+export default async function TransactionsPage({
+  searchParams,
+}: {
+  searchParams: { regionCode?: string; tradeDate?: string };
+}) {
+  const { regionCode, tradeDate } = searchParams;
+
+  if (!regionCode || !tradeDate) {
+    return null;
+  }
+
+  const data = await getTransactions(regionCode, tradeDate);
+
+  if (!data) {
+    return null;
+  }
+
   return (
     <main className="container mx-auto py-8">
-      <section className="flex flex-col gap-y-8">
-        <div className="flex flex-col gap-y-5">
-          <Suspense fallback={<div>Loading...</div>}>
-            <TransactionSearch />
-          </Suspense>
-          <Suspense fallback={<div>Loading...</div>}>
-            <TransactionList />
-          </Suspense>
-        </div>
-      </section>
+      <div className="flex flex-col gap-y-5">
+        <TransactionSearch />
+        <TransactionList regionCode={regionCode} data={data} />
+      </div>
     </main>
   );
 }
