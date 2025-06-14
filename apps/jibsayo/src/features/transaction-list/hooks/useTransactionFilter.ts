4a3983d61e2f765b2e6edf6176ab18d36655ff76
pys99pys <pys99pys@gmail.com>
2025-06-15 00:49:45 +0900

    검색 데이터 변경시 필터링 정보 초기화

--- apps/jibsayo/src/features/transaction-list/hooks/useTransactionFilter.ts
+++ apps/jibsayo/src/features/transaction-list/hooks/useTransactionFilter.ts
@@ -1,38 +1,47 @@
+import { STORAGE_KEY } from '@/shared/consts/storageKey';
+import { getItem, setItem } from '@/shared/lib/sessionStorage';
+
 import { useEffect, useState } from 'react';
 
 import { TransactionFilter } from '../models/types';
-
-const STORAGE_KEY = 'transaction-filter';
+import { useSearchParamChange } from './useSearchParamChange';
 
 interface Return {
   filter: TransactionFilter;
   setFilter: (value: Partial<TransactionFilter>) => void;
 }
 
-export function useTransactionFilter(): Return {
-  const [filterState, setFilterState] = useState<TransactionFilter>(() => {
-    const storedFilter = sessionStorage.getItem(STORAGE_KEY);
-    if (storedFilter) {
-      return JSON.parse(storedFilter);
-    }
-    return {
-      apartName: '',
-      isNationalSizeOnly: false,
-      isFavoriteOnly: false,
-    };
-  });
+const initialState: TransactionFilter = {
+  apartName: '',
+  isNationalSizeOnly: false,
+  isFavoriteOnly: false,
+};
+
+export const useTransactionFilter = (): Return => {
+  const [filterState, setFilterState] =
+    useState<TransactionFilter>(initialState);
 
   useEffect(() => {
-    sessionStorage.setItem(STORAGE_KEY, JSON.stringify(filterState));
-  }, [filterState]);
+    const storedFilter = getItem<TransactionFilter>(
+      STORAGE_KEY.TRANSACTION_LIST_FILTER
+    );
+    if (storedFilter) {
+      setFilterState(storedFilter);
+    }
+  }, []);
+
+  useSearchParamChange(() => {
+    setFilterState(initialState);
+  });
 
   const setFilter = (nextFilter: Partial<TransactionFilter>) => {
     const changedFilter = { ...filterState, ...nextFilter };
     setFilterState(changedFilter);
+    setItem(STORAGE_KEY.TRANSACTION_LIST_FILTER, changedFilter);
   };
 
   return {
     filter: filterState,
     setFilter: setFilter,
   };
-}
+};
