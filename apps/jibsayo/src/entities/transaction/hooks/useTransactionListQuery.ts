3999d6ca940aaa98c29ec362d25c43fa191af9e3
pys99pys <pys99pys@gmail.com>
2025-06-16 00:46:39 +0900

    조회 데이터 로컬 저장: 세션 스토리지를 활용한 거래 내역 데이터 캐싱 구현

--- apps/jibsayo/src/entities/transaction/hooks/useTransactionListQuery.ts
+++ apps/jibsayo/src/entities/transaction/hooks/useTransactionListQuery.ts
@@ -1,5 +1,13 @@
 import { TransactionsResponse } from '@/app/api/transactions/types';
-import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { STORAGE_KEY } from '@/shared/consts/storageKey';
+import { getItem, setItem } from '@/shared/lib/sessionStorage';
+import {
+  useQuery,
+  useQueryClient,
+  UseQueryResult,
+} from '@tanstack/react-query';
+
+import { useEffect, useMemo, useState } from 'react';
 
 import { useSearchParams } from './useSearchParams';
 
@@ -8,11 +16,34 @@ export const useTransactionListQuery = (): UseQueryResult<
   unknown
 > => {
   const { searchParams } = useSearchParams();
+  const [isInitialized, setIsInitialized] = useState(false);
+  const queryClient = useQueryClient();
 
-  return useQuery({
-    queryKey: ['transactionList', searchParams],
+  const queryKey = useMemo(
+    () => ['transactionList', searchParams],
+    [searchParams]
+  );
+
+  const storageKey = useMemo(
+    () =>
+      `${STORAGE_KEY.TRANSACTION_LIST_RESPONSE}__${searchParams.regionCode}__${searchParams.tradeDate}`,
+    [searchParams.regionCode, searchParams.tradeDate]
+  );
+
+  // 초기 데이터 로드
+  useEffect(() => {
+    const storedData = getItem<TransactionsResponse>(storageKey);
+    if (storedData) {
+      queryClient.setQueryData(queryKey, storedData);
+    }
+    setIsInitialized(true);
+  }, [searchParams.regionCode, searchParams.tradeDate]);
+
+  const query = useQuery({
+    queryKey,
     staleTime: 1000 * 60 * 60,
     gcTime: 1000 * 60 * 60,
+    enabled: isInitialized,
     queryFn: async () => {
       const response = await fetch(
         `/api/transactions?area=${searchParams.regionCode}&createDt=${searchParams.tradeDate}`
@@ -20,4 +51,16 @@ export const useTransactionListQuery = (): UseQueryResult<
       return response.json();
     },
   });
+
+  // 데이터 저장
+  useEffect(() => {
+    if (!query.data) return;
+
+    const hasStorageData = getItem<TransactionsResponse>(storageKey);
+    if (!hasStorageData) {
+      setItem(storageKey, query.data);
+    }
+  }, [query.data, storageKey]);
+
+  return query;
 };
