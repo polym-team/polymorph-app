c8ac593ead4fa25ba46ae4d3edc012f509733c94
pys99pys <pys99pys@gmail.com>
2025-06-18 00:46:03 +0900

    소소한 UI 수정

--- apps/jibsayo/src/features/apart-detail/ui/ApartInfo.tsx
+++ apps/jibsayo/src/features/apart-detail/ui/ApartInfo.tsx
@@ -34,14 +34,19 @@ export function ApartInfo({
       </Typography>
 
       {infoItems.map((item, index) => (
-        <div key={index} className="flex border-t py-2 last:pb-0">
-          <div className="w-1/4 py-2 lg:w-40 xl:w-64">
-            <Typography variant="small" className="font-medium text-gray-500">
+        <div key={index} className="flex items-center border-t py-2 last:pb-0">
+          <div className="w-[110px] py-2 lg:w-40 xl:w-64">
+            <Typography
+              variant="small"
+              className="text-sm font-medium text-gray-500"
+            >
               {item.label}
             </Typography>
           </div>
           <div className="flex-1">
-            <Typography className="font-medium">{item.value || '-'}</Typography>
+            <Typography className="text-sm font-medium">
+              {item.value || '-'}
+            </Typography>
           </div>
         </div>
       ))}
