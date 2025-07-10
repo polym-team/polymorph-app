ac4d456237723e92f33c811bc56a135994a8ddb3
pys99pys <pys99pys@gmail.com>
2025-07-10 20:25:07 +0900

    push-client 관련 export 제거

--- packages/firebase/src/index.ts
+++ packages/firebase/src/index.ts
@@ -1,8 +1,7 @@
 // 메인 클래스 export
 export { AdminFirestoreClient } from './admin-client';
-export { PushNotificationClient } from './push-client';
+export { ExpoPushNotificationClient } from './expo-push-client';
 
 // 유틸리티 함수들 export
 export * from './utils';
 export * from './types';
-export * from './push-client';
