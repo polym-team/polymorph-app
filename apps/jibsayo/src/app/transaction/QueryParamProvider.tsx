3022678aa6fe581ddc790e5bf9dc730eb93494e9
pys99pys <pys99pys@gmail.com>
2025-10-14 17:03:31 +0900

    쿼리 파라미터 저장 로직 추가

---
+++ apps/jibsayo/src/app/transaction/QueryParamProvider.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import {
+  SearchParams,
+  useTransactionPageSearchParams,
+} from '@/entities/transaction';
+import { STORAGE_KEY } from '@/shared/consts/storageKey';
+import { useOnceEffect } from '@/shared/hooks';
+import { getItem, setItem } from '@/shared/lib/sessionStorage';
+
+import { useEffect, useState } from 'react';
+
+interface QueryParamProviderProps {
+  children: React.ReactNode;
+}
+
+export function QueryParamProvider({ children }: QueryParamProviderProps) {
+  const { searchParams, setSearchParams } = useTransactionPageSearchParams();
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    const isEmpty = !searchParams.regionCode || !searchParams.tradeDate;
+    if (isEmpty) return;
+
+    setItem(STORAGE_KEY.TRANSACTION_QUERY_PARAMS, searchParams);
+  }, [searchParams]);
+
+  useOnceEffect(!!searchParams.regionCode && !!searchParams.tradeDate, () => {
+    setIsInitialized(true);
+  });
+
+  useOnceEffect(true, () => {
+    const savedSearchParams = getItem<SearchParams>(
+      STORAGE_KEY.TRANSACTION_QUERY_PARAMS
+    );
+
+    if (savedSearchParams) {
+      setSearchParams({
+        ...savedSearchParams,
+        maxSize: savedSearchParams.maxSize ?? Infinity,
+      });
+    }
+  });
+
+  if (!isInitialized) return null;
+
+  return children;
+}
--- apps/jibsayo/src/app/transaction/page.tsx
+++ apps/jibsayo/src/app/transaction/page.tsx
@@ -3,13 +3,17 @@ import { TransactionSearch } from '@/features/transaction-search';
 
 import { Suspense } from 'react';
 
+import { QueryParamProvider } from './QueryParamProvider';
+
 export default async function TransactionPage() {
   return (
-    <div className="flex flex-col gap-y-5">
-      <Suspense fallback={null}>
-        <TransactionSearch />
-        <TransactionList />
-      </Suspense>
-    </div>
+    <Suspense fallback={null}>
+      <QueryParamProvider>
+        <div className="flex flex-col gap-y-5">
+          <TransactionSearch />
+          <TransactionList />
+        </div>
+      </QueryParamProvider>
+    </Suspense>
   );
 }
