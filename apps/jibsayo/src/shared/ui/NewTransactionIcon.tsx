b21546355bbcfaa8b854a1534151845a8181c587
pys99pys <pys99pys@gmail.com>
2025-12-02 10:02:44 +0900

    refactor: NEW 아이콘 수정

--- apps/jibsayo/src/shared/ui/NewTransactionIcon.tsx
+++ apps/jibsayo/src/shared/ui/NewTransactionIcon.tsx
@@ -4,7 +4,7 @@ export function NewTransactionIcon() {
       <span className="bg-danger text-primary-foreground flex h-4 items-center justify-center rounded-[4px] px-1 pt-[1px] text-[9px]">
         NEW
       </span>
-      <span className="absolute left-1/2 top-full -translate-x-1/2">
+      <span className="absolute left-1/2 top-full -translate-x-1/2 inline-block">
         <span className="border-t-danger border-l-[3px] border-r-[3px] border-t-[3px] border-l-transparent border-r-transparent"></span>
       </span>
     </span>
